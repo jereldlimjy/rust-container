68abb052bfec0611e263e3395f885d31d6e33ba5
Jereld Lim Jun You <jereldlimjy@hotmail.com>
2024-10-08 20:24:47 +0800

    add run function and match on first arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if let Some(first_arg) = args.get(1) {
+        match first_arg.as_str() {
+            "run" => run(&args[2..]),
+            _ => panic!("{first_arg} is not a valid argument"),
+        }
+    } else {
+        panic!("what?");
+    }
+}
+
+fn run(args: &[String]) {
+    if let Some(command) = args.get(0) {
+        Command::new(command)
+            .args(&args[1..])
+            .spawn()
+            .expect("command failed to start");
+    } else {
+        panic!("no command provided");
+    }
 }
